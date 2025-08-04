2fbbe0e67389f7c874ee9a90841b34c3bd601b60
Abdirahman136896 <ogle_abdirahman@students.uonbi.ac.ke>
2025-08-04 17:12:15 +0300

    first commit

---
+++ Server/config/database.js
@@ -0,0 +1,6 @@
+const dbconfig = {
+    url : "mongodb+srv://ecommerce:Abdul1342@cluster0.mzedxfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" , 
+    dbname : 'test'
+}
+
+module.exports = dbconfig ; 
